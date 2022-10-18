cbc8aee13b5de185587bf63fdb88d9129724ff61
AmabileForster <111066186+AmabileForster@users.noreply.github.com>
2022-10-18 13:46:53 -0300

    Add files via upload

---
+++ src/pages/login/login.jsx
@@ -0,0 +1,14 @@
+import { Grid } from "@mui/material";
+
+const Login = () => {
+    return <Grid container spacing={2}>
+                <Grid item xs={0} md={7} lg={8}>
+                    <img src="https://vitapet.com/media/sptf1jzm/most-popular-puppy-and-kitten-names-2021-1240x640.jpg?anchor=center&mode=crop&width=1240&rnd=132834911121830000" />
+                </Grid>
+                <Grid item xs={12} md={5} lg={4}>
+                    
+                </Grid>
+            </Grid>
+}
+
+export default Login;
